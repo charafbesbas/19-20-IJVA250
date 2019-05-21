5089c0d41795f1853ae38b50a1ff9bcc74d62d1e
charaf.besbas <charafbesbas@hotmail.fr>
2019-05-21 14:58:23 +0200

    test

--- src/test/java/com/example/demo/FactureTest.java
+++ src/test/java/com/example/demo/FactureTest.java
@@ -1,9 +1,15 @@
 package com.example.demo;
 
+import com.example.demo.entity.Facture;
 import org.junit.Test;
 
 public class FactureTest {
 
     @Test
-    public void getTotal_factureVideSansArticleRetourneZero () {}
+    public void getTotal_factureVideSansArticleRetourneZero () {
+
+        Facture facture = new Facture();
+
+        Double total = facture.getTotal();
+    }
 }
